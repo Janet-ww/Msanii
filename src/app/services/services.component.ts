e618d829678ef710e9f96f5e4d72b236813d06ed
Gachuhi.Chiuri <gachuhi.karanja@eclectics.io>
2023-10-24 08:26:30 +0300

    - Removed some hardcoded HTML Data and moved it to the TS file.
    - Used directives like *ngFor to loop through my array of services
    
    - Utilized two way binding [routerLink] . This is to attach dynamic values to HTML element properties

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -3,13 +3,42 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  styleUrls: ['./services.component.css'],
 })
 export class ServicesComponent implements OnInit {
+  servicesData: any[] = [
+    {
+      serviceName: 'Accounting and Tax Services',
+      pageLink: '/accounting',
+      imageUrl: 'assets/images/services/accounting.jpg',
+      serviceList: [
+        'Bookkeeping',
+        'Financial Statement Preparation',
+        'Income Tax Filing',
+        'Payroll Management',
+        'Small Business Accounting',
+        'Tax Planning and Consultation',
+        'Estate and Inheritance Planning',
+        'Audit and Assurance',
+      ],
+    },
+    {
+      serviceName: 'Beauty and Personal Care',
+      pageLink: '/beauty',
+      imageUrl: 'assets/images/services/beautycare.jpg',
+      serviceList: [
+        'Barber Services',
+        'Facials and Skincare Treatments',
+        'Hair Braiding and Styling',
+        'Hair Removal (Waxing, Threading)',
+        'Makeup Artistry for Special Occasions',
+        'Manicure and Pedicure Services',
+        'Spa Services',
+      ],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
